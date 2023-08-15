1a46b4cd8c4fb6de067b7358ee954a72049c53d3
Shivansh Raghav <shivanshraghav7@gmail.com>
2023-08-15 12:22:45 +0000

    sub

--- src/components/ToolTip.js
+++ src/components/ToolTip.js
@@ -5,7 +5,7 @@ let ToolTip= (props)=>{
 return(
     <div>
        { hover && (<h2 className='tooltiptext'><div>{props.text}</div></h2>) }
-        <p className='tootip' onMouseOver={()=>Sethover(true)} onMouseOut={()=>Sethover(false)}><div>{props.child}</div></p>
+        <p className='tooltip'><div onMouseEnter={()=>Sethover(true)} onMouseLeave={()=>Sethover(false)}>{props.child}</div></p>
     </div>
 )
 }
